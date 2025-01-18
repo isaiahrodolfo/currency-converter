9002bf94b0ff61046bce515f710c0b88fbfcfda8
Isaiah <irodol@uw.edu>
2025-01-17 22:41:03 -0800

    Fetch API based on inputs

--- src/App.js
+++ src/App.js
@@ -1,22 +1,50 @@
+import { useState, useEffect } from "react";
+
 // `https://api.frankfurter.app/latest?amount=100&from=EUR&to=USD`
 
 export default function App() {
+  const [currencyBefore, setCurrencyBefore] = useState("");
+
+  const [currencyFrom, setCurrencyFrom] = useState("USD");
+  const [currencyTo, setCurrencyTo] = useState("EUR");
+
+  const [multiplier, setMultiplier] = useState("");
+
+  useEffect(() => {
+    const getMultiplier = async () => {
+      const res = await fetch(`https://api.frankfurter.app/latest?amount=${currencyBefore}&from=${currencyFrom}&to=${currencyTo}`);
+      const data = await res.json();
+      setMultiplier(data.rates[currencyFrom]);
+    }
+    getMultiplier();
+  }, [currencyBefore, currencyFrom, currencyTo])
+
   return (
     <div>
-      <input type="text" />
-      <select>
+      <input
+        type="number"
+        value={currencyBefore}
+        onChange={(e) => setCurrencyBefore(e.target.value)}
+      />
+      <select
+        value={currencyFrom}
+        onChange={(e) => setCurrencyFrom(e.target.value)}
+      >
         <option value="USD">USD</option>
         <option value="EUR">EUR</option>
         <option value="CAD">CAD</option>
         <option value="INR">INR</option>
       </select>
-      <select>
+      <select
+        value={currencyTo}
+        onChange={(e) => setCurrencyTo(e.target.value)}
+      >
         <option value="USD">USD</option>
         <option value="EUR">EUR</option>
         <option value="CAD">CAD</option>
         <option value="INR">INR</option>
       </select>
-      <p>OUTPUT</p>
+      <p>{multiplier}</p>
     </div>
   );
 }
